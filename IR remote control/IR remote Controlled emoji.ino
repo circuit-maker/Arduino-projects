e1d1207dbac99b25f100b602a05f0c175a55a935
circuit-maker <alldetails123@gmail.com>
2026-06-02 16:11:58 +0530

    IR remote Controlled emoji.ino

--- IR remote control/IR remote Controlled emoji.ino
+++ IR remote control/IR remote Controlled emoji.ino
@@ -1,1 +1,35 @@
 
+#include <IRremote.hpp>
+#include <Servo.h>
+
+#define IR_PIN 5
+
+Servo myservo;
+
+void setup() {
+  Serial.begin(9600);
+
+  IrReceiver.begin(IR_PIN, ENABLE_LED_FEEDBACK);
+
+  myservo.attach(9);
+  myservo.write(90);   // start center position
+}
+
+void loop() {
+
+  if (IrReceiver.decode()) {
+
+    unsigned long value = IrReceiver.decodedIRData.decodedRawData;
+
+    Serial.println(value, HEX);
+
+    // ====== BUTTON 1 ======
+    if (value == 0xFD02BF00) {   // replace with your HEX
+      myservo.write(0);
+    }
+        // ====== BUTTON 7 ======
+    if (value == 0xF708BF00) {   // replace with your HEX
+      myservo.write(180);
+     }
+    IrReceiver.resume();
+  }
